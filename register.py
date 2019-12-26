b0071fa411d9651910cf8780655b5bc86a8abfb6
Akshay <akshaytolwanipython@gmail.com>
2019-12-26 19:32:39 +0530

    fixed bugs

--- register.py
+++ register.py
@@ -1,23 +1,25 @@
 from selenium import webdriver
+from time import  sleep
 
-# Open codeforces in Chrome
+# Open codeforces contests in Chrome
 browser = webdriver.Chrome()
-browser.get('https://codeforces.com/register')
+browser.get('https://codeforces.com/contests')
 
-#Submit handle
-handle = browser.find_element_by_name('handle')
-handle.send_keys('akhaxx')
+#Click on the register now button in the attention tab via copy full xxpath in developer tools
+contests = browser.find_element_by_xpath("/html/body/div[5]/div[4]/div[1]/div/div[4]/div[1]/a[2]")
+contests.click()
+sleep(10)
 
-#Submit email
-email = browser.find_element_by_name('email')
-email.send_keys('gwyeqfg@hfvoefuho.tk')
+handle_or_email = browser.find_element_by_xpath('/html/body/div[5]/div[4]/div/div/div/form/table/tbody/tr[1]/td[2]/input')
+login1 = input('Please input your codeforces handle/email: ')
+handle_or_email.send_keys(login1)
 
-#Submit password
-password = browser.find_element_by_name('password')
-password.send_keys('Tatasky98')
+password = browser.find_element_by_xpath('/html/body/div[5]/div[4]/div/div/div/form/table/tbody/tr[2]/td[2]/input')
+login2 = input("Please input your codeforces password: ")
+password.send_keys(login2)
 
-#Submit confirm_password
-confirm_password = browser.find_element_by_name('passwordConfirmation')
-confirm_password.send_keys('Tatasky98')
-
-confirm_password.submit()
+submit = browser.find_element_by_xpath('/html/body/div[5]/div[4]/div/div/div/form/table/tbody/tr[4]/td/div[1]/input')
+submit.click()
+sleep(10)
+register = browser.find_element_by_xpath('/html/body/div[5]/div[4]/div[2]/form/table/tbody/tr[3]/td/div/input')
+register.click()
